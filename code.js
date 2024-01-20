a6031a7d3ac30105527e4b8889986e5ec218c796
Romain <romamour04@gmail.com>
2024-01-20 01:25:38 +0100

    first functions

---
+++ code.js
@@ -0,0 +1,5 @@
+function addition(x, y) {
+    return x + y;
+}
+var result = addition(10, 20);
+console.log(result);
--- code.ts
+++ code.ts
@@ -1,5 +1,5 @@
-function addition(x, y) {
+function addition(x:number , y:number):number {
     return x + y;
 }
-const result = addition('10', '20');
+const result:number = addition(10, 20);
 console.log(result);
